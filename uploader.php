edc55a394793343b1dc0621c0e700604b65a3374
sean1138 <sean@vsxd.com>
2024-12-15 20:11:41 -0500

    favicon

--- uploader.php
+++ uploader.php
@@ -7,6 +7,7 @@ require_once 'uploader-validate-multi-user.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="upload-svgrepo-com.svg">
 	<title>File Uploader</title>
 	<style>
 		body {
